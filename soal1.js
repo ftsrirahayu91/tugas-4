50e57f07cafd4554445f04032653e9996a490bc0
ftsrirahayu91 <ftsrirahayu91@gmail.com>
2022-02-02 13:38:08 -0800

    tugas4

---
+++ soal1.js
@@ -0,0 +1,12 @@
+
+function trafficlight (lampu){
+    if (lampu == "red"){
+        console.log("stop");
+    } 
+    else if (lampu == "yellow"){
+        console.log("be carefull!");
+    }
+    else if (lampu == "green"){
+        console.log("go!");
+    }
+}
---
+++ soal4.js
@@ -0,0 +1,17 @@
+
+const obj = {
+    nama: "sri rahayu",
+    kelas: "RPL 1",
+    umur: 17, 
+    nikah: false,
+    Hp: {
+      nama: "OPPO A15",
+      spesifikasiHp: {
+          chipset: "Snapdragon 720G",
+          ram: "3 GB",
+          rom: "32 GB",
+          camera: "64mp"
+          }
+      }
+    }
+  console.log(obj.Hp);
